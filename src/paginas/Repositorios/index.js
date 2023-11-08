ba9448932aae2720811c6494c837d6242c5ce962
Antony Maziero Warken <againplayi7@gmail.com>
2023-11-08 14:29:08 -0300

    cap 3 finalizado

--- src/paginas/Repositorios/index.js
+++ src/paginas/Repositorios/index.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, FlatList, TouchableOpacity } from 'react-native';
 import estilos from './estilos';
+import { pegarRepositoriosDoUsuario } from '../../servicos/requisicoes/repositorios';
+import { useIsFocused } from '@react-navigation/native';
 
 export default function Repositorios({ route, navigation }) {
     const [repo, setRepo] = useState([]);
+    const estaNaTela = useIsFocused()
+
+    useEffect( async () => {
+        const resultado = await pegarRepositoriosDoUsuario(route.params.id)
+        setRepo(resultado)
+    },[estaNaTela])
 
     return (
         <View style={estilos.container}>
@@ -14,6 +22,22 @@ export default function Repositorios({ route, navigation }) {
                 >
                     <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
                 </TouchableOpacity>
+
+                <FlatList
+                    data={repo}
+                    style={{ width: '100%' }}
+                    keyExtractor={repo => repo.id}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity 
+                            style={estilos.repositorio}
+                            onPress={() => navigation.navigate('InfoRepositorio', {item})}
+                        >
+                            <Text style={estilos.repositorioNome}>{item.name}</Text>
+                            <Text style={estilos.repositorioData}>Atualizado em {item.data}</Text>
+                        </TouchableOpacity>
+                    )}
+                />
+
         </View>
     );
 }
